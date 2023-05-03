cc1c67d06a36c56696a3617e713306cf2e7e47bd
codcod <nikos@linux.com>
2023-05-03 22:08:34 +0200

    fmt

--- web-rust/src/services.rs
+++ web-rust/src/services.rs
@@ -4,9 +4,9 @@ pub(crate) use actix_web::{
     web::{Data, Json, Path},
     HttpResponse, Responder,
 };
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use sqlx::{self, FromRow};
-use chrono::NaiveDate;
 
 #[derive(Serialize, FromRow)]
 struct Author {
@@ -67,17 +67,18 @@ pub async fn create_author_book(
     let id: i32 = path.into_inner();
 
     match sqlx::query_as::<_, Book>(
-        "INSERT INTO books (title, summary, publication_date, author_id) VALUES ($1, $2, $3, $4) RETURNING book_id, title, summary, publication_date, author_id"
+        "INSERT INTO books (title, summary, publication_date, author_id) VALUES ($1, $2, $3, $4)
+        RETURNING book_id, title, summary, publication_date, author_id",
     )
-        .bind(body.title.to_string())
-        .bind(body.summary.to_string())
-        .bind(body.publication_date)
-        .bind(id)
-        .fetch_one(&state.db)
-        .await
+    .bind(body.title.to_string())
+    .bind(body.summary.to_string())
+    .bind(body.publication_date)
+    .bind(id)
+    .fetch_one(&state.db)
+    .await
     {
         Ok(book) => HttpResponse::Ok().json(book),
-        Err(_) => HttpResponse::InternalServerError().json("Failed to create a book and add it to author"),
+        Err(_) => HttpResponse::InternalServerError().json("Failed to create a book for an author"),
         //Err(error) => panic!("error: {}", error),
     }
 }
